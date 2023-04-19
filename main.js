bed86a255620f52d5096bc86debc49e8c9b8490d
shalalalaliala <106209223+shalalalaliala@users.noreply.github.com>
2023-04-18 18:38:43 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(900,600);
+    canvas.position(200,200);
+}
+
+function draw(){
+fill(0,0,200);
+stroke(230,0,0);
+circle(50,50,80);
+circle(50,430,80);
+circle(590,430,80);
+circle(590,50,80);
+fill(0,0,200);
+stroke(230,0,0);
+rect(90,40,460,20);
+rect(90,420,460,20);
+rect(40,90,20,300);
+rect(580,90,20,300);
+}
+
+
+
